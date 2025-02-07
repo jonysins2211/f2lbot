38c634d5e1d6b4f7a8989601d03207fbf11e005d
jonysins2211 <Jonysins11220@gmail.com>
2025-02-07 19:14:57 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -20,7 +20,7 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002005906539'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002005906539'))
-    PORT = int(getenv('PORT', '9055')) #(Here, enter the hosting port you received from HostingUp, which should start From 9000)
+    PORT = int(getenv('PORT', '8089')) #(Here, enter the hosting port you received from HostingUp, which should start From 9000)
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0')) #(DO NOT CHANGE IF U DEPLOY YOUR BOT ON HOSTINGUP VPS)
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "949657126").split()]
@@ -33,14 +33,14 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', 'https://www.movieloverzbotz.us.kg/') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '5xserver.hostingup.icu:8089/') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     HAS_SSL = False
-    FQDN = 'https://www.movieloverzbotz.us.kg/'#(Here, enter the domain and hosting port you received from HostingUp. If you're using your own, enter that, but make sure to use the domain name, not the IP address, and include the port as well.)
+    FQDN = '5xserver.hostingup.icu:8089/'#(Here, enter the domain and hosting port you received from HostingUp. If you're using your own, enter that, but make sure to use the domain name, not the IP address, and include the port as well.)
     if HAS_SSL:
-        URL = "https://www.movieloverzbotz.us.kg/".format(FQDN)
+        URL = "5xserver.hostingup.icu:8089/".format(FQDN)
     else:
-        URL = "https://www.movieloverzbotz.us.kg/".format(FQDN)
+        URL = "5xserver.hostingup.icu:8089/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://primebaby11220:TuGa0I5ZsiC8mp5A@cluster0.df4rivj.mongodb.net/?retryWrites=true&w=majorit'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'MovieEntertainment4u')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
